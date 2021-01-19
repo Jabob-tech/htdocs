1dc5b6c443b5781030ab0d0598fbe3a83f0a740d
Jabob-tech <jakub.ozdzynski28@gmail.com>
2021-01-19 20:18:19 +0100

    Update index.php

--- html/php/index.php
+++ html/php/index.php
@@ -5,6 +5,14 @@
     <title></title>
   </head>
   <body>
+    <h1>Zamówienie</h1>
+    <form class="" action="index.html" method="post">
+      <label for="pc_count">Ilość komputerów (3499zł)</label>
+      <input type="number" name="pc_count" value=""><br><br>
+      <label for="monitor_count">Ilość monitorów (699zł)</label>
+      <input type="number" name="monitor_count" value=""><br><br>
+      <button type="submit" name="submit_button">Wyślij</button>
+    </form>
     <?php
     $var = "Hello World!";
     echo "$var, to znaczy Witaj Świecie!";
