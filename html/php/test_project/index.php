5b6649fe5b47add16ebd12e51c7b2f3dfb66ee26
Jabob-tech <jakub.ozdzynski28@gmail.com>
2021-01-20 19:18:24 +0100

    adding login form in index.php

--- html/php/test_project/index.php
+++ html/php/test_project/index.php
@@ -5,6 +5,15 @@
     <title>Strona logowania</title>
   </head>
   <body>
-
+    <form action="order.php" method="post">
+      <h1 class="form-label">Zaloguj się</h1><hr>
+      <label for="email" class="input-label">Nazwa użytkownika lub email <br><br>
+        <input type="text" name="email" value="" placeholder="email" class="form-input" required>
+      </label>
+      <label for="password" class="input-label">Hasło <br><br>
+        <input type="password" name="password" placeholder="hasło"class="form-input" required>
+      </label>
+      <button type="submit" name="send-button" value="" class="send-button">zaloguj się</button>
+    </form>
   </body>
 </html>
