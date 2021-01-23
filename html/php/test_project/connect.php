b74b9ee4f66a351d34a2c3c3a9c4dcc8fa388d91
Jabob-tech <jakub.ozdzynski28@gmail.com>
2021-01-23 18:26:22 +0100

    First successful connection with database

---
+++ html/php/test_project/connect.php
@@ -0,0 +1,7 @@
+<?php
+//here are the most important data only to connect with database
+  $host = "localhost";
+  $db_user = "root";
+  $db_password = ""; //database password, basically there's no password in phpmyadmin
+  $db_name = "portfoliodb"
+ ?>
--- html/php/test_project/sign.php
+++ html/php/test_project/sign.php
@@ -1,6 +1,20 @@
 <?php
+  //this file handles signing in
+  require_once "connect.php";
 
-  $login = $_POST['login_input'];
-  $password = $_POST['password_input'];
+  $connection = @new mysqli($host, $db_user, $db_password, $db_name);
 
+  if($connection ->connect_errno!=0)
+  {
+    echo "Error: ".$connection->connect_errno;
+  }
+  else
+  {
+    $login = $_POST['login_input'];
+    $password = $_POST['password_input'];
+    $sql = "SELECT * FROM users WHERE username ='$login' AND password='$password'";
+
+
+    $connection -> close();
+  }
  ?>
