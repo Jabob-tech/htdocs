8f40543103173e6f8923570e7b7c4160ef521e9a
Jabob-tech <jakub.ozdzynski28@gmail.com>
2021-01-23 18:52:25 +0100

    Update sign.php

--- html/php/test_project/sign.php
+++ html/php/test_project/sign.php
@@ -4,15 +4,22 @@
 
   $connection = @new mysqli($host, $db_user, $db_password, $db_name);
 
-  if($connection ->connect_errno!=0)
-  {
+  if($connection ->connect_errno!=0) {
     echo "Error: ".$connection->connect_errno;
   }
-  else
-  {
+  else {
     $login = $_POST['login_input'];
     $password = $_POST['password_input'];
     $sql = "SELECT * FROM users WHERE username ='$login' AND password='$password'";
+
+  if($result = @$connection -> query($sql)) {
+    $how_many_users = $result -> num_rows;
+    if ($how_many_users>0) {
+      $record = $result ->fetch_assoc();
+    }
+    else {
+    }
+  }
 
 
     $connection -> close();
