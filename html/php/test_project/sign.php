210acb3920a0d85daa19cc502ce86c24bb425d8b
Jabob-tech <jakub.ozdzynski28@gmail.com>
2021-01-26 19:42:17 +0100

    Update sign.php

--- html/php/test_project/sign.php
+++ html/php/test_project/sign.php
@@ -17,9 +17,10 @@
 
     $login = htmlentities($login, ENT_QUOTES, "UTF-8");
     $password = htmlentities($password, ENT_QUOTES, "UTF-8");
-    $sql = "SELECT * FROM users WHERE username ='$login' AND password='$password'";
 
-  if($result = @$connection -> query($sql)) {
+  if($result = @$connection -> query(sprintf("SELECT * FROM users WHERE username ='%s' AND password='%s'",
+  mysqli_real_escape_string($connection,$login),
+  mysqli_real_escape_string($connection,$password)))) {
     $how_many_users = $result -> num_rows;
     if ($how_many_users>0) {
       $_SESSION['signed_in'] = true;
